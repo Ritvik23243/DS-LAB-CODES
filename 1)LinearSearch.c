a37bac565f4208ffd5026c2be4a515a23d15297e
Ritvik23243 <146938036+Ritvik23243@users.noreply.github.com>
2023-10-04 23:14:56 +0530

    Update 1)LinearSearch.c

--- 1)LinearSearch.c
+++ 1)LinearSearch.c
@@ -1,67 +1,84 @@
 #include<stdio.h>
 int linearsearchiterative(int *,int,int);
 int linearsearchrecursive(int *,int,int,int);
-int main(){
+int main()
+{
     printf("1)Iterative Linear Search\n");
     printf("2)Recursive Linear Search\n");
     printf("Enter your choice:");
     int choice,index;
     scanf("%d",&choice);
-    if(choice<1&&choice>2){
+    if(choice<1&&choice>2)
+    {
         printf("Invalid Choice");
     }
-    else{
+    else
+    {
         printf("Enter the no of elements required:");
         int n;
         scanf("%d",&n);
         int arr[n];
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             printf("Enter the element of the array:");
             scanf("%d",&arr[i]);
         }
         printf("Enter the element to search for:");
         int element;
         scanf("%d",&element);
-        switch(choice){
+        switch(choice)
+        {
             case 1:
                 index=linearsearchiterative(arr,element,n);
-                if(index==-1){
+                if(index==-1)
+                {
                     printf("Element not found");
                 }
-                else{
+                else
+                {
                     printf("Element found at index %d",index);
                 }
                 break;
             case 2:
                 index=linearsearchrecursive(arr,element,n,0);
-                if(index==-1){
+                if(index==-1)
+                {
                     printf("Element not found");
                 }
-                else{
+                else
+                {
                     printf("Element found at index %d",index);
                 }
                 break;            
         }
     }
 }
-int linearsearchiterative(int *arr,int element,int size){
+int linearsearchiterative(int *arr,int element,int size)
+{
     int index=-1;
-    for(int i=0;i<size;i++){
-        if(element==arr[i]){
+    for(int i=0;i<size;i++)
+    {
+        if(element==arr[i])
+        {
             return i;
         }
     }
     return index;
 }
-int linearsearchrecursive(int *arr,int element,int size,int start){
-    if(start==size){
+int linearsearchrecursive(int *arr,int element,int size,int start)
+{
+    if(start==size)
+    {
         return -1;
     }
-    else{
-        if(arr[start]==element){
+    else
+    {
+        if(arr[start]==element)
+        {
             return start;
         }
-        else{
+        else
+        {
             start++;
             return linearsearchrecursive(arr,element,size,start);
         }
